0111b7901562990e63e8524e2f417d1fd941ba35
Irina Varushkina <73347907+Vistp@users.noreply.github.com>
2021-09-08 16:26:33 +0500

    Удаляет создание разметки, комментарии

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// вариант с динамическим созданием разметки творого и последующих таймеров,
-// в разметке нужно закомментировать второй таймер, либо заменить id timer-2 на другой
-const bodyEl = document.querySelector('body');
-
 class CountdownTimer {
     constructor(selector, targetDate) {
         this.selector = selector;
         this.targetDate = targetDate;
-        // this.onTick = onTick;
   }
     start() {
         setInterval(() => {
@@ -14,7 +9,7 @@ class CountdownTimer {
             const currentTime = Date.now();
             const deltaTime = this.targetDate - currentTime;
             const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
-            console.log(`${days}:${hours}:${mins}:${secs}`, this.selector);
+            // console.log(`${days}:${hours}:${mins}:${secs}`, this.selector);
             this.updateClockface({ days, hours, mins, secs });
         }, 1000);
     }
@@ -39,39 +34,11 @@ class CountdownTimer {
     hoursEl.textContent = `${hours}:`;
     minsEl.textContent = `${mins}:`;
     secsEl.textContent = secs;
-    
 }
 };
-
-function createTimer(timerName) {
-    return `
-    <div class="timer" id="${this.selector}">
-        <div class="field">
-            <span class="value" data-value="days">11</span>
-            <span class="label">Days</span>
-        </div>
-    
-        <div class="field">
-            <span class="value" data-value="hours">11</span>
-            <span class="label">Hours</span>
-        </div>
-    
-        <div class="field">
-            <span class="value" data-value="mins">11</span>
-            <span class="label">Minutes</span>
-        </div>
-    
-        <div class="field">
-            <span class="value" data-value="secs">11</span>
-            <span class="label">Seconds</span>
-        </div>
-    </div>
-    `
-}
 
 const timerOne = new CountdownTimer('timer-1', new Date('Sep 17, 2021'));
 timerOne.start();
 
 const timerTwo = new CountdownTimer('timer-2', new Date('Oct 17, 2021'));
 timerTwo.start();
-bodyEl.insertAdjacentHTML('beforeend', createTimer.bind(timerTwo)());
